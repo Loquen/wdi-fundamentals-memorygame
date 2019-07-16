bd47d31ee2d3da3eb10c583fce2ab5a904a5ac36
Loquen Jones <lrjones416@gmail.com>
2019-07-15 17:25:39 -0700

    created function flipCard and checkForMatch

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,25 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert('You found a match');
-	} else {
-		alert('Sorry, No match');
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert('You found a match');
+		} else {
+			alert('Sorry, No match');
+		}
 	}
 }
 
-console.log("User flipped " + cardOne);
+function flipCard(cardId){
+	console.log("User flipped: " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+
+
+flipCard(2);
+flipCard(3);
